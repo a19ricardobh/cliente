9a39fb192f76e2e065a71e641d9b95c25c594b82
a19ricardobh <a19ricardobh@iessanclemente.net>
2022-10-20 20:28:52 +0200

    jueves 20oct

--- Ejercicios-Javascript-Tanda 4/Ejercicio-032/scripts/myscript.js
+++ Ejercicios-Javascript-Tanda 4/Ejercicio-032/scripts/myscript.js
@@ -11,3 +11,26 @@
 *
 *
 ***************************************************************************************************************/
+const array=[52,58,50,54,53,51,57,55,59]
+const a2=[3,5,8,4,2,1,7,10,9]
+
+
+//const found = array.find(element => element[array.indexOf(element)+1] == element+1);
+//console.log(found)
+function buscaPerdido(array){
+    array.sort(function(a, b) {
+        if (a>b)
+            return 1
+        if (a<b)
+            return -1
+      })
+    //console.log(array)
+    for (let index = 0; index < array.length-1; index++) {
+        if (array[index+1]!=array[index]+1){
+            return array[index]+1
+        }
+    }
+}
+
+console.log(buscaPerdido(array))
+console.log(buscaPerdido(a2))
--- Ejercicios-Javascript-Tanda 4/Ejercicio-033/scripts/myscript.js
+++ Ejercicios-Javascript-Tanda 4/Ejercicio-033/scripts/myscript.js
@@ -12,3 +12,49 @@
 *             ¿Podríamos resolverlo empleando recursividad?
 *
 ***************************************************************************************************************/
+const c1="Dabale arroz a la zorra el abad"
+const c2="Dab rroz a fla zorra el abad"
+//metodo clasico
+/*
+function esPalindromo(cadena) {
+    const array=cadena.toLowerCase().replaceAll(' ','').split("")
+    for (let i = 0,j=array.length-1; i <= j; i++,j--) {
+        if (array[i]!=array[j]){
+            return "La cadena no es un palíndromo"
+        }
+    }
+    return "La cadena es un palíndromo"
+}*/
+
+//con metodos de array
+/*
+function esPalindromo(cadena) {
+    const array=cadena.toLowerCase().replaceAll(' ','').split("")
+    const arrayR=array.slice()
+    arrayR.reverse()
+    console.log(array)
+    console.log(arrayR)
+    if (array.toString()!==arrayR.toString()){
+        return "La cadena no es un palíndromo"
+    }else{
+        return "La cadena es un palíndromo"
+    }
+}*/
+
+//recursivo
+function esPalindromo(cadena) {
+    const cad=cadena.toLowerCase().replaceAll(' ','')
+    
+        if (cad[0]!=cad[cad.length-1])
+            return false
+        else {
+            if (cad.length>1) 
+                return esPalindromo(cad.substring(1,cad.length-1))  
+            else
+                return true
+        }
+}
+
+
+console.log(esPalindromo(c1))
+console.log(esPalindromo(c2))
