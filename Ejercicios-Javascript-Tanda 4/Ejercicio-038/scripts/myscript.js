8e4e59f475701e4b4769f46cf1bb3dc03d10c9ac
a19ricardobh <a19ricardobh@iessanclemente.net>
2022-10-29 17:48:05 +0200

    ejercicio 38

--- Ejercicios-Javascript-Tanda 4/Ejercicio-038/scripts/myscript.js
+++ Ejercicios-Javascript-Tanda 4/Ejercicio-038/scripts/myscript.js
@@ -11,3 +11,58 @@
 *
 *
 ***************************************************************************************************************/
+function leerDatos(mensaje){
+    let i=true
+    let n=prompt(mensaje)
+    do{
+        if (isNaN(n)) {
+            n=prompt(mensaje)
+        }else{
+            i=false
+            return parseInt(n)
+        }
+    }while(i)
+}
+
+const dia=leerDatos("Escribir el dia")
+const mes=leerDatos("EScribir el mes")
+const ano=leerDatos("Escribir el año")
+
+function validaFecha(d,m,a){
+    const mes31=[1,3,5,7,8,10,12]
+    const mes30=[4,6,9,11]
+    valida=true
+    //validar mes
+    if (m<1 || m>12){
+        return false
+    }
+    //validar dia
+    if (mes31.includes(m)){ //meses de 31 dias
+        if (d<1 || d>31) {
+            return false
+        }
+    } else if (mes30.includes(m)){ //meses de 30 dias
+        if (d<1 || d>30) {
+            return false
+        }
+    } else if (m==2){ //febrero
+        if (a%4==0 && a%400==0) { //año bisiesto
+            if (d<1 || d>29) {
+                return false
+            }
+        }else{
+            if (d<1 || d>28) {
+                return false
+            } 
+        }
+    }
+    return true
+}
+
+
+
+if (validaFecha(dia,mes,ano)){
+    console.log(`La fecha ${dia}/${mes}/${ano} es correcta`)
+}else{
+    console.log(`La fecha ${dia}/${mes}/${ano} no es correcta`)
+}
