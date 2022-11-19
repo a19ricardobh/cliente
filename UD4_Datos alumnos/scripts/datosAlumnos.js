3c80ba8c2010cb52bb14c9444d51dae1a910f039
a19ricardobh <a19ricardobh@iessanclemente.net>
2022-11-19 18:31:43 +0100

    datos alumnos

---
+++ UD4_Datos alumnos/scripts/datosAlumnos.js
@@ -0,0 +1,8 @@
+const $d=document,
+      $sexo=$d.getElementsByName("sexo"),
+      $estudios=$d.querySelector("#estudios"),
+      $alumnos=$d.querySelectorAll(".campo")
+
+console.log($sexo)
+console.log($estudios)
+console.log($alumnos)
