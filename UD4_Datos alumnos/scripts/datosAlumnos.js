3ff0c1b5a066c3bc3690b10ec45ad1a59168e24c
a19ricardobh <a19ricardobh@iessanclemente.net>
2022-11-20 15:33:26 +0100

    datos alumnos-casa

--- UD4_Datos alumnos/scripts/datosAlumnos.js
+++ UD4_Datos alumnos/scripts/datosAlumnos.js
@@ -1,8 +1,29 @@
 const $d=document,
       $sexo=$d.getElementsByName("sexo"),
       $estudios=$d.querySelector("#estudios"),
-      $alumnos=$d.querySelectorAll(".campo")
+      $alumnos=$d.querySelectorAll(".campo"),
+      $submit=$d.querySelector(".boton"),
+      $tabla=$d.querySelector(".estilo-tabla")
 
-console.log($sexo)
-console.log($estudios)
-console.log($alumnos)
+//console.log($sexo)
+//console.log($estudios)
+//console.log($tabla)
+
+
+
+$submit.addEventListener("click",event=>{
+    let $tr="",$td=""
+    $tr=$d.createElement("tr")
+    $alumnos.forEach(e=>{
+        //console.log(e.getElementsByTagName("input")[0].value)
+        
+        $td=$d.createElement("td")
+        $tr.appendChild($td)
+        $td.append(e.getElementsByTagName("input")[0].value)
+    
+        
+     })
+     $tabla.appendChild($tr)
+     
+    
+})
