9f2d7de3f32ff692763f80d36fbe0fe1334d3662
a19ricardobh <a19ricardobh@iessanclemente.net>
2022-11-25 13:28:42 +0100

    Examen Domino - casa

--- Ejercicio_Examen_Domino/scripts/myscript.js
+++ Ejercicio_Examen_Domino/scripts/myscript.js
@@ -1,2 +1,37 @@
-n = 1 + 2+ 3+ ...+p
-despues si nos pasamos de n, volvemos a hacerlo desde n =2+3+...+p
+let n=8 //numero de fichas
+let filas=0
+let control=true
+//primera fila 
+let fichasCuspide=1
+let fichasBase=1
+let fichasRestantes=n
+
+function imprimir(n,filas,fichasCuspide,fichasBase){
+    console.log(`Con ${n} fichas de dominó Blanca puede crear una pirámide de altura ${filas} con 
+    ${fichasBase} fichas en la base y ${fichasCuspide} fichas en la cúspide`)
+}
+
+function calculoPiramide(n,filas,fc) {
+    fichasCuspide=fc    
+    return n-fichasBase
+}
+
+while(control){
+    if(fichasRestantes==0){
+        imprimir(n,filas,fichasCuspide,fichasBase-1)
+        control=false
+    }else if (fichasRestantes>0){
+        fichasRestantes=calculoPiramide(fichasRestantes,filas,fichasCuspide)   
+        fichasBase++
+        filas++
+    }else {
+        filas=0
+        fichasRestantes=n
+        fichasCuspide++
+        fichasBase=fichasCuspide
+        calculoPiramide(n,filas,fichasCuspide)
+    }
+}
+
+
+
