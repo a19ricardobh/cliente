f5cd1ac28845e37054244e2cbb7050bbb88c6c7d
a19ricardobh <a19ricardobh@iessanclemente.net>
2023-02-08 20:13:56 +0100

    DOM app tareas 8 feb

--- UD6_DOM - Tareas App - Alumnos/scripts/script2.js
+++ UD6_DOM - Tareas App - Alumnos/scripts/script2.js
@@ -3,28 +3,57 @@ const $d=document,
       $tarea=$d.querySelector("#input"),
       $boton=$formulario.querySelector("button"),
       $content=$d.querySelector("#tareas"),
-      $template=$d.querySelector("#template-tarea")
+      $template=$d.querySelector("#template-tarea").content
 
 let tareas=[]
 
 function renderTareas(){
     $content.innerHTML=""
-    const $clone=$template.cloneNode(true)
-    $p=$clone.querySelector("p")
-    console.log($p)
-    $i=$clone.querySelectorAll("i")
     tareas.forEach((element ,index)=> {
-        $p.textContent=element
+        const $clone=$template.cloneNode(true)
+
+        $p=$clone.querySelector("p")
+        $i=$clone.querySelectorAll("i")
+        $p.textContent=element.tarea
         $i[0].dataset.tarea=index
         $i[1].dataset.tarea=index
+        //toggle pone o quita la clase del classList, segun el segundo parametro sea true o false
+        $clone.querySelector("div").classList.toggle("realizada",element.activo)
+        console.log($i[0].classList)
+        $i[0].classList.toggle("fa-check-circle",!element.activo)
+        $i[0].classList.toggle("fa-undo-alt",element.activo)
         $content.appendChild($clone)
-    });
+    });  
+}
+
+function eliminarTarea(indice){
+    tareas.splice(indice,1)
+    renderTareas()
+}
+
+function marcarTarea(indice){
+    if (tareas[indice].activo){
+        tareas[indice].activo=false
+    }else{
+        tareas[indice].activo=true
+    }
     
+    renderTareas()
 }
 
 $formulario.addEventListener("submit",e=>{
     e.preventDefault()
     
-    tareas.push($tarea.value)
+    tareas.push({tarea:$tarea.value,activo:false})
+    
     renderTareas()
+})
+
+$content.addEventListener("click",e=>{
+    if (e.target.classList.contains("text-success")){
+        marcarTarea(e.target.dataset.tarea)
+    }
+    if (e.target.classList.contains("text-danger")){
+        eliminarTarea(e.target.dataset.tarea)
+    }
 })
