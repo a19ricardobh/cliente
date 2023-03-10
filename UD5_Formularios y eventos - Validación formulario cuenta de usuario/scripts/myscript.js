cf9eb915887dad49bc294789e2328529073cc73a
a19ricardobh <a19ricardobh@iessanclemente.net>
2023-03-10 14:33:53 +0100

    validacion formulario cuenta

--- UD5_Formularios y eventos - Validación formulario cuenta de usuario/scripts/myscript.js
+++ UD5_Formularios y eventos - Validación formulario cuenta de usuario/scripts/myscript.js
@@ -2,6 +2,7 @@ const $d=document,
     $formulario=$d.querySelector("#formulario"),
     $boton=$d.querySelector("button")
 
+const expRegNom=/[a-z]+/i
 
 
 $formulario.addEventListener("click",e=>{
@@ -12,12 +13,28 @@ $formulario.addEventListener("click",e=>{
         let email=$d.querySelector("#email")
         let pass=$d.querySelector("#pass")
         let passConf=$d.querySelector("#usuario")
-        if (user.value==""){
-            user.setAttribute("class","falla")
-        // } || email.value=="" || pass.value=="" || passConf.value==""){
+        if (!expRegNom.test(user.value) ){
+            if(!user.parentElement.className.includes("falla")){
+                user.parentElement.setAttribute("class","form-control falla")
+                user.nextElementSibling.append("Campo vacío")
+            }
+        }else{
+            user.parentElement.setAttribute("class","form-control ok")
+            user.nextElementSibling.innerHTML=""
+        }
+        if (email.value==""){
+            if(!email.parentElement.className.includes("falla")){
+                email.parentElement.setAttribute("class","form-control falla")
+                email.nextElementSibling.append("Campo vacío")
+            } 
+        }else{
+            email.parentElement.setAttribute("class","form-control ok")
+            email.nextElementSibling.innerHTML=""
+        }
+        // }  pass.value=="" || passConf.value==""){
             
 
-        }
+        
 
 
 
