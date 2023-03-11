b4f43c3578950416cc78f445519262b27a54443c
a19ricardobh <a19ricardobh@iessanclemente.net>
2023-03-11 17:23:55 +0100

    validacion formulario 11 marzo

--- UD5_Formularios y eventos - Validación formulario cuenta de usuario/scripts/myscript.js
+++ UD5_Formularios y eventos - Validación formulario cuenta de usuario/scripts/myscript.js
@@ -2,7 +2,8 @@ const $d=document,
     $formulario=$d.querySelector("#formulario"),
     $boton=$d.querySelector("button")
 
-const expRegNom=/[a-z]+/i
+const expRegNom=/[a-z]+/i,
+      expRegPas=/[a-z]+/i
 
 
 $formulario.addEventListener("click",e=>{
@@ -13,6 +14,7 @@ $formulario.addEventListener("click",e=>{
         let email=$d.querySelector("#email")
         let pass=$d.querySelector("#pass")
         let passConf=$d.querySelector("#usuario")
+        //Validacion campo usuario
         if (!expRegNom.test(user.value) ){
             if(!user.parentElement.className.includes("falla")){
                 user.parentElement.setAttribute("class","form-control falla")
@@ -22,6 +24,7 @@ $formulario.addEventListener("click",e=>{
             user.parentElement.setAttribute("class","form-control ok")
             user.nextElementSibling.innerHTML=""
         }
+        //validacion campo email
         if (email.value==""){
             if(!email.parentElement.className.includes("falla")){
                 email.parentElement.setAttribute("class","form-control falla")
@@ -31,12 +34,29 @@ $formulario.addEventListener("click",e=>{
             email.parentElement.setAttribute("class","form-control ok")
             email.nextElementSibling.innerHTML=""
         }
-        // }  pass.value=="" || passConf.value==""){
-            
+        //Validacion del campo password
+        if (!expRegPas.test(pass.value) ){
+            if(!pass.parentElement.className.includes("falla")){
+                pass.parentElement.setAttribute("class","form-control falla")
+                pass.nextElementSibling.append("Campo vacío")
+            }
+        }else{
+            pass.parentElement.setAttribute("class","form-control ok")
+            pass.nextElementSibling.innerHTML=""
+        }    
 
-        
-
-
-
+        //Validacion confirmacion del password
+        if (pass.value!=""){
+            if (passConf.value=="" || passConf.value!=pass.value){
+                if(!passConf.parentElement.className.includes("falla")){
+                    passConf.parentElement.setAttribute("class","form-control falla")
+                    passConf.nextElementSibling.append("el password no concuerda")
+                } 
+            }else{
+                    passConf.parentElement.setAttribute("class","form-control ok")
+                    passConf.nextElementSibling.innerHTML=""
+            }
+        }
     }
+
 })
