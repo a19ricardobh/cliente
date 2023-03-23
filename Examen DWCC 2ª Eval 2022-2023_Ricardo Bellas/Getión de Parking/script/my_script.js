bb9f2105dfb39810424c83d997b7718655d5289b
a19ricardobh <a19ricardobh@iessanclemente.net>
2023-03-23 19:24:24 +0100

    Examen 2eval continuacion

--- Examen DWCC 2ª Eval 2022-2023_Ricardo Bellas/Getión de Parking/script/my_script.js
+++ Examen DWCC 2ª Eval 2022-2023_Ricardo Bellas/Getión de Parking/script/my_script.js
@@ -21,79 +21,98 @@ const nFilas = 3,
   nCols = 3;
 const matriculas=[{
     id:1,
-    placa:"libre"
+    placa:"libre",
+    hora:""
   },
   {
     id:2,
-    placa:"libre"
+    placa:"libre",
+    hora:""
   },
   {
     id:3,
-    placa:"libre"
+    placa:"libre",
+    hora:""
   },
   {
     id:4,
-    placa:"libre"
+    placa:"libre",
+    hora:""
   },
   {
     id:5,
-    placa:"libre"
+    placa:"libre",
+    hora:""
   },
   {
     id:6,
-    placa:"libre"
+    placa:"libre",
+    hora:""
   },
   {
     id:7,
-    placa:"libre"
+    placa:"libre",
+    hora:""
   },
   {
     id:8,
-    placa:"libre"
+    placa:"libre",
+    hora:""
   },
   {
     id:9,
-    placa:"libre"
+    placa:"libre",
+    hora:""
   },
   {
     id:10,
-    placa:"libre"
+    placa:"libre",
+    hora:""
   },
   {
     id:11,
-    placa:"libre"
+    placa:"libre",
+    hora:""
   },
   {
     id:12,
-    placa:"libre"
+    placa:"libre",
+    hora:""
   },
   {
     id:13,
-    placa:"libre"
+    placa:"libre",
+    hora:""
   },
   {
     id:14,
-    placa:"libre"
+    placa:"libre",
+    hora:""
   },
   {
     id:15,
-    placa:"libre"
+    placa:"libre",
+    hora:""
   },
   {
     id:16,
-    placa:"libre"
+    placa:"libre",
+    hora:""
   },
   {
     id:17,
-    placa:"libre"
+    placa:"libre",
+    hora:""
   },
   {
     id:18,
-    placa:"libre"
+    placa:"libre",
+    hora:""
   },
   {
     id:19,
-    placa:"libre"
+    placa:"libre",
+    hora:""
   }
 ]
 let plazas=19
@@ -106,19 +125,28 @@ const $d=document,
 
 const expMat=/[A-Z]{3}-[0-9]{3}/
 
-function agregaMatricula(matricula){
+function agregaMatricula(matricula,hora){
   //console.log(matriculas)
   //buscar el primer sitio libre y si es primo
   let pos=matriculas.find(e=>e.placa=="libre" && esPrimo(e.id))
   if (pos){
     pos.placa=matricula
+    pos.hora=hora
     plazas--
   }else{
     let pos2=matriculas.find(e=>e.placa=="libre")
     pos2.placa=matricula
+    pos.hora=hora
     plazas--
   }
   
+}
+
+function elimina(num){
+  let elimina=matriculas.find(e=>e.id==num)
+  elimina.placa="libre"
+  elimina.hora=""
+  plazas++
 }
 
 function existePlaca(texto){
@@ -161,8 +189,12 @@ function render(array){
     let small=$d.createElement("small")
     small.append(e.placa)
     td.appendChild(small)
+    let small2=$d.createElement("small")
+    small2.append(e.hora)
+    td.appendChild(small2)
     let boton=$d.createElement("button")
     boton.append("Eliminar")
+    boton.dataset.clave=e.id
     td.appendChild(boton)
     $tabla.appendChild(td)
   })
@@ -172,15 +204,27 @@ function render(array){
 $form.addEventListener("submit",e=>{
   e.preventDefault()
   let matricula = $form.querySelector("input").value
+  let hor= new Date().getHours()
+  
+  let min= new Date().getMinutes()
+  const hora=`${hor}:${min}`
+  
   if(expMat.test(matricula) && existePlaca(matricula) && libre()){
-    agregaMatricula(matricula)
+    agregaMatricula(matricula,hora)
     render(matriculas)
   }else{
     alert("error en los datos")
   }
 })
 
-
+$tabla.addEventListener("click",e=>{
+  e.preventDefault()
+  if (e.target.dataset.clave){
+   
+    elimina(e.target.dataset.clave)
+    render(matriculas)
+  }
+})
 
 
 
